49d54278c13ae7aa85d92bc4b62cdf5506b7ce02
abacus2021 <sonu.kumar@abacusdesk.in>
2023-05-29 11:18:38 +0530

    change type plumber to crpenter

--- src/_services/DatabaseService.ts
+++ src/_services/DatabaseService.ts
@@ -21,9 +21,9 @@ export class DatabaseService implements OnInit {
  
 
     // <------------------ Test Link ------------------------------>
-    dbUrl = "http://bonus.basiq360.com/api/index.php/";
-    uploadUrl = "http://bonus.basiq360.com/api/uploads/";
-    downloadUrl = "http://bonus.basiq360.com/api/uploads/Download_excel/"; 
+    dbUrl = "https://bonus.basiq360.com/api/index.php/"; 
+    uploadUrl = "https://bonus.basiq360.com/api/uploads/";
+    downloadUrl = "https://bonus.basiq360.com/api/uploads/Download_excel/"; 
 
     // <------------------ Live Link ------------------------------>
     // dbUrl = "https://pearl.abacusdesk.com/api/index.php/"; 
