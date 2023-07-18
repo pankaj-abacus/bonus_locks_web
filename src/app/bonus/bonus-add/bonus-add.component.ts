3cac91285835631823c4a01f2b670ff58fd407d4
vishal kumar <vishal.kumarabacusdesk@gmail.com>
2023-07-18 17:06:21 +0530

    chnages

--- src/app/bonus/bonus-add/bonus-add.component.ts
+++ src/app/bonus/bonus-add/bonus-add.component.ts
@@ -25,21 +25,21 @@ export class BonusAddComponent implements OnInit {
   form_statelist: any = [];
   form_districtlist:any =[];
   areaInfluencer:any=[];
-  
-  constructor(public service:DatabaseService, public session: sessionStorage, public rout: Router, public toast:ToastrManager) { 
+
+  constructor(public service:DatabaseService, public session: sessionStorage, public rout: Router, public toast:ToastrManager) {
     this.minDate = new Date();
     this.assign_login_data = this.session.getSession();
     this.logined_user_data = this.assign_login_data.value.data;
     this.getInfluencer();
   }
-  
+
   ngOnInit() {
   }
-  
+
   getState(){
-    
+
     this.service.post_rqst({},'Bonus/getAllState').subscribe((result)=>
-    {   
+    {
       if (result['statusCode'] == 200){
         this.states=result['all_state'];
       }
@@ -48,14 +48,14 @@ export class BonusAddComponent implements OnInit {
       }
     }, error => {
     })
-    
+
   }
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
   storestate(state_name) {
     if(state_name) {
       this.form_statelist.push({state_name: state_name});
@@ -65,24 +65,24 @@ export class BonusAddComponent implements OnInit {
     var x = this.form_statelist.findIndex(items => items.state_name === state_name);
     if(x  != '-1')this.form_statelist.splice(x, 1);
   }
-  
+
   storedistrict(stateinp,district_name) {
     if(district_name) {
       this.form_districtlist.push({'state_name':stateinp,'district_name':district_name});
     }
   }
-  
+
   removeDistrictListData(district_name) {
     var x = this.form_districtlist.findIndex(items => items.district_name === district_name);
     if(x  != '-1')this.form_districtlist.splice(x, 1);
   }
-  
+
   removeDist(stateinput){
     var x = this.districts.findIndex(items => items.state_name === stateinput);
     if(x  != '-1')this.districts.splice(x, 1);
-    
+
   }
-  
+
   getDistrictList(stateinput,e){
     if(e){
       this.districtList(stateinput);
@@ -118,8 +118,8 @@ export class BonusAddComponent implements OnInit {
       }
     }
   }
-  
-  
+
+
   newDistrict:any= []
   districtList(stateinput) {
     this.service.post_rqst({'state_name':stateinput}, "Bonus/getAllDistrict").subscribe((result => {
@@ -130,13 +130,13 @@ export class BonusAddComponent implements OnInit {
       else{
         this.toast.errorToastr(result['statusMsg']);
       }
-      
-      
+
+
     }));
-    
+
   }
-  
-  
+
+
   getSelDistrict(stateinp,districtinput,e){
     if(e){
       this.storedistrict(stateinp,districtinput);
@@ -144,15 +144,15 @@ export class BonusAddComponent implements OnInit {
       this.removeDistrictListData(districtinput);
     }
   }
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
   pointCategory_data(status)
   {
-    this.filter.point_type = status; 
+    this.filter.point_type = status;
     this.service.post_rqst({'filter':this.filter},'Bonus/pointCategoryMasterList').subscribe((result)=>
     {
       if (result['statusCode'] == 200){
@@ -163,11 +163,11 @@ export class BonusAddComponent implements OnInit {
       }
     })
   }
-  
-  
+
+
   getInfluencer()
   {
-    this.filter.scanning_rights = 'Yes'; 
+    this.filter.scanning_rights = 'Yes';
     this.service.post_rqst({'filter':this.filter},'Bonus/influencerMasterList').subscribe((result)=>
     {
       if (result['statusCode'] == 200){
@@ -178,15 +178,15 @@ export class BonusAddComponent implements OnInit {
       }
     })
   }
-  
-  
+
+
   getAreaInfluencer(search){
     if(this.form_districtlist.length == 0){
       return;
     }
     else{
       this.service.post_rqst({'user_type':this.data.types, 'search':search, 'influencer_type':this.data.influencer_type,'state':this.form_statelist,'district':this.form_districtlist},'Bonus/influencerList').subscribe((result)=>
-      {   
+      {
         if (result['data']['statusCode'] == 200){
           this.areaInfluencer = result['data']['result'];
         }
@@ -196,9 +196,9 @@ export class BonusAddComponent implements OnInit {
       }, error => {
       })
     }
-    
+
   }
-  
+
   selInfluncer:any =[];
   selState:any =[];
   setInfluencer(e, id){
@@ -210,7 +210,7 @@ export class BonusAddComponent implements OnInit {
       this.selInfluncer.splice(removeindex, 1);
     }
   }
-  
+
   allInfluncer(){
     if( !this.data.Influencer ){
       this.selInfluncer= [];
@@ -222,17 +222,19 @@ export class BonusAddComponent implements OnInit {
       for (let i = 0; i < this.states.length; i++) {
         this.areaInfluencer[i].selected = true;
         this.selInfluncer.push({'id': this.areaInfluencer[i].id});
-        
+
       }
     }
   }
-  
-  
+
+
   submitDetail() {
-    
+
+
     let productPoint =[]
     for (let i = 0; i < this.pointCategories_data.length; i++) {
       const element = this.pointCategories_data[i];
+
       productPoint.push({'product_id':element.id, 'product_name':element.point_category_name, 'influencer_point':element.scheme_influencer_point})
     }
     this.data.start_date = this.data.start_date  ? this.service.pickerFormat(this.data.start_date) : '';
@@ -257,6 +259,6 @@ export class BonusAddComponent implements OnInit {
       }
     });
   }
-  
-  
+
+
 }
