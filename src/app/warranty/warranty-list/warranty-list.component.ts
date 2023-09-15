5a9d38f6535cefb85e44b5d2c848e8a56a593f90
Mitesh-Abacus <mitesh@abacusdesk.co.in>
2023-09-15 18:47:24 +0530

    Make changes in service module

--- src/app/warranty/warranty-list/warranty-list.component.ts
+++ src/app/warranty/warranty-list/warranty-list.component.ts
@@ -14,7 +14,7 @@ import { WarrantyUpdateModelComponent } from '../warranty-update-model/warranty-
   styleUrls: ['./warranty-list.component.scss']
 })
 export class WarrantyListComponent implements OnInit {
-  
+
   fabBtnValue: any = 'add';
   warrantyList: any = [];
   active_tab: any = 'Pending';
@@ -23,6 +23,7 @@ export class WarrantyListComponent implements OnInit {
   page_limit: any;
   start: any = 0;
   count: any;
+  all_count: any;
   tab_count: any;
   total_page: any = 0;
   pagenumber: any = 0;
@@ -36,47 +37,47 @@ export class WarrantyListComponent implements OnInit {
   sr_no: number;
   datanotofound: boolean = false;
   downurl: any = ''
-  
+
   constructor(public dialog: DialogComponent, public dialogs: MatDialog, public alert: DialogComponent, public service: DatabaseService, public rout: Router, public toast: ToastrManager, public session: sessionStorage) {
     this.downurl = service.downloadUrl
     this.page_limit = service.pageLimit;
     this.getWarrantyList('');
     console.log(this.service.pageLimit);
     console.log(this.page_limit);
-    
-    
-    
+
+
+
   }
-  
+
   ngOnInit() {
     this.filter_data = this.service.getData()
 
     if (this.filter_data.status) {
       this.active_tab = this.filter_data.status
     }
-    
+
   }
-  
+
   pervious() {
     this.start = this.start - this.page_limit;
     this.getWarrantyList('');
   }
-  
+
   nextPage() {
     this.start = this.start + this.page_limit;
     this.getWarrantyList('');
   }
-  
+
   refresh() {
     this.start = 0;
     this.filter_data = {};
     this.getWarrantyList('');
   }
-  
+
   clear() {
     this.refresh();
   }
-  
+
   goToDetailHandler(id) {
     window.open(`/customer-detail/` + id);
   }
@@ -84,12 +85,12 @@ export class WarrantyListComponent implements OnInit {
     this.filter_data.date_created = moment(this.filter_data.date_created).format('YYYY-MM-DD');
     this.getWarrantyList('');
   }
-  
+
   date_format2(): void {
     this.filter_data.date_of_purchase = moment(this.filter_data.date_of_purchase).format('YYYY-MM-DD');
     this.getWarrantyList('');
   }
-  
+
   date_format3(): void {
     this.filter_data.warranty_end_date = moment(this.filter_data.warranty_end_date).format('YYYY-MM-DD');
     this.getWarrantyList('');
@@ -99,7 +100,7 @@ export class WarrantyListComponent implements OnInit {
     this.filter_data.verification_on = moment(this.filter_data.verification_on).format('YYYY-MM-DD');
     this.getWarrantyList('');
   }
-  
+
   getWarrantyList(data) {
     if (this.pagenumber > this.total_page) {
       this.pagenumber = this.total_page;
@@ -108,8 +109,8 @@ export class WarrantyListComponent implements OnInit {
     if (this.start < 0) {
       this.start = 0;
     }
-    
-    
+
+
 
     if (this.active_tab == 'All') {
     this.filter_data.status = this.active_tab;
@@ -126,23 +127,24 @@ export class WarrantyListComponent implements OnInit {
       this.filter_data.status = this.active_tab;
     }
     console.log(this.page_limit);
-    
+
     let header = this.service.post_rqst({ 'filter': this.filter_data, 'start': this.start, 'pagelimit': this.page_limit }, "ServiceTask/serviceWarrantyList")
-    
+
     this.loader = true;
     header.subscribe((result) => {
       if (result['statusCode'] == 200) {
-        
+
         console.log('result',result);
-        
-        
+
+
         this.warrantyList = result['result'];
         console.log(this.warrantyList);
-        
+
         this.pageCount = result['count'];
         console.log(this.pageCount);
-        
+
         this.tab_count = result['tab_count'];
+        this.all_count = result['tab_count']['all_count'];
         console.log(this.tab_count);
 
         this.loader = false;
@@ -152,7 +154,7 @@ export class WarrantyListComponent implements OnInit {
           this.datanotofound = true;
           this.loader = false;
         }
-        
+
         if (this.pagenumber > this.total_page) {
           this.pagenumber = this.total_page;
           this.start = this.pageCount - this.page_limit;
@@ -163,8 +165,8 @@ export class WarrantyListComponent implements OnInit {
         this.total_page = Math.ceil(this.pageCount / this.page_limit);
         this.sr_no = this.pagenumber - 1;
         this.sr_no = this.sr_no * this.page_limit
-        
-        
+
+
         for (let i = 0; i < this.warrantyList.length; i++) {
           if (this.warrantyList[i].status == '1') {
             this.warrantyList[i].newStatus = true
@@ -179,7 +181,7 @@ export class WarrantyListComponent implements OnInit {
         this.datanotofound = true;
         this.loader = false;
       }
-      
+
     })
   }
   lastBtnValue(value) {
@@ -195,15 +197,15 @@ export class WarrantyListComponent implements OnInit {
   //         id: row,
   //       }
   //     });
-      
+
   //     dialogRef.afterClosed().subscribe(result => {
   //       if (result != false) {
   //         // this.getComplaintDetail();
   //       }
   //     });
   //   }
-  
-  
+
+
   downloadExcel() {
     this.service.post_rqst({ 'filter': this.filter_data }, "Excel/service_warranty_list").subscribe((result => {
       if (result['msg'] == true) {
@@ -213,5 +215,5 @@ export class WarrantyListComponent implements OnInit {
       }
     }));
   }
-  
+
 }
